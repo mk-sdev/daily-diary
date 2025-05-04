eb190e5fa565d2a355dbacc186251a57051428c0
Mikołaj <kaleta222@gmail.com>
2025-05-04 22:06:18 +0200

    update UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,6 +9,7 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function HomeScreen() {
   const [notes, setNotes] = useState<{ date: string; text: string }[]>([])
@@ -24,52 +25,36 @@ export default function HomeScreen() {
   const navigation = useNavigation()
   // AsyncStorage.clear()
   return (
-    <View>
+    <SafeAreaView>
       <FlatList
         data={notes}
         keyExtractor={item => item.date}
         renderItem={({ item }) => (
           <TouchableOpacity
-            onPress={() => {
-              // Serializowanie obiektu do JSON i kodowanie go w URL
-              const noteString = encodeURIComponent(JSON.stringify(item))
-              router.push(`/explore?note=${noteString}`)
-            }}
+            // onPress={() => {
+            //   // Serializowanie obiektu do JSON i kodowanie go w URL
+            //   const noteString = encodeURIComponent(JSON.stringify(item))
+            //   router.push(`/explore?note=${noteString}`)
+            // }}
           >
             <View
               style={{
-                elevation: 2,
-                backgroundColor: 'beige',
+                elevation: 1,
+                backgroundColor: 'rgb(230, 230, 230)',
                 borderWidth: 1,
+                borderColor: 'rgb(190, 190, 190)',
                 margin: 10,
                 padding: 5,
+                borderRadius: 10,
+                paddingHorizontal: 10
               }}
             >
-              <Text>{item.date}</Text>
+              <Text style={{fontWeight: 'bold'}}>{item.date}</Text>
               <Text>{item.text}</Text>
             </View>
           </TouchableOpacity>
         )}
       />
-    </View>
+    </SafeAreaView>
   )
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-})
